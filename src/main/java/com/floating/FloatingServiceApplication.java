c0f2d92f4acad3ded0dc5b0f59ce44b60b57b612
yuhou.wang <952945457@qq.com>
2023-08-16 18:27:10 +0800

    Initial commit

---
+++ src/main/java/com/floating/FloatingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.floating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloatingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloatingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/floating/FloatingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.floating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloatingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
